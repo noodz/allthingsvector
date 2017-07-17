29f6f2ba619d705a07065d37bfcdc6ffd520c29d
noodz <atinc@ualberta.ca>
2017-07-16 18:12:13 -0600

    Update system of linear equations.py

--- system of linear equations.py
+++ system of linear equations.py
@@ -4,9 +4,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 def sofleq(example):
-    x,y = example
-    for i,l in enumerate(range(0,y)):
+    
+    for i,l in enumerate(x,y):
         fig,ax = plt.subplots(figsize=(10,10))
         plt.xlim (-10,10)
         plt.ylim (-10,10)
@@ -18,11 +22,11 @@ def sofleq(example):
             ax.spines[spine].set_color('none')
         ax.grid(axis=10)
         ax.set_title('Vectors & Vector ops')
-        ax.arrow(0,0,example[i,0],example[i,1],head_width =0.5, head_length = 0.5)
+        ax.arrow(0,0,x[i,0],y[i,1],head_width =0.5, head_length = 0.5)
         plt.show()
         
-x, y = np.array([[1,-1],[1,1]]), np.array([1,3])
-example = np.linalg.solve(x,y)
+u, v = np.array([[1,-1],[1,1]]), np.array([1,3])
+x, y = np.linalg.solve(u,v)
+print(u,v)
 print(x,y)
-print(example)
 sofleq(example)
